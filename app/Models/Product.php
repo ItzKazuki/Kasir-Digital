199bd6c1cdff61b82b85ea6e269074b3f77391e4
ItzKazuki <ibnu235729@gmail.com>
2025-02-19 19:33:29 +0700

    feat: define all necessary relationships in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'discount_id',
+        'price',
+        'stock',
+        'image_url',
+        'description',
+    ];
+
+    // Relasi: Produk milik satu kategori
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relasi: Produk bisa memiliki satu diskon (opsional)
+    public function discount()
+    {
+        return $this->belongsTo(Discount::class);
+    }
+
+    // Relasi: Produk bisa masuk dalam banyak order detail
+    public function orderDetails()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
 }
