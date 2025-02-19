ac33543dcf3625be0f002fa80b4ff68f4d806523
ItzKazuki <ibnu235729@gmail.com>
2025-02-19 19:27:40 +0700

    feat: define all necessary relationships in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,50 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'member_id',
+        'order_date',
+        'total_items',
+        'total_price',
+        'discount_id',
+    ];
+
+    // Relasi: Order dibuat oleh satu user (kasir/admin)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relasi: Order dibuat oleh satu member (opsional)
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
+
+    // Relasi: Order bisa memiliki satu diskon (opsional)
+    public function discount()
+    {
+        return $this->belongsTo(Discount::class);
+    }
+
+    // Relasi: Satu order bisa memiliki banyak order detail
+    public function orderDetails()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
+
+    // Relasi: Satu order memiliki satu transaksi
+    public function transaction()
+    {
+        return $this->hasOne(Transaction::class);
+    }
 }
+
