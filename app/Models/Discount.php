36f6911cf3ad83288ec58a31b03b3edbf850766d
ItzKazuki <ibnu235729@gmail.com>
2025-02-19 19:37:20 +0700

    feat: add Discount-Product, Discount-Order relationship and add mutator to set field value to round while field type is percentage

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Discount extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'type',
+        'value',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+
+    public function setValueAttribute($value)
+    {
+        $this->attributes['value'] = ($this->attributes['type'] === 'percentage') ? round($value) : $value;
+    }
+
+    // Relasi: Satu diskon bisa berlaku untuk banyak produk
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    // Relasi: Satu diskon bisa berlaku untuk banyak order
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
