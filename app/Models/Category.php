58c10c799cf975152a7211c0d9ee20207c64d8cc
ItzKazuki <ibnu235729@gmail.com>
2025-02-19 19:35:47 +0700

    feat: add Category-Product relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    // Relasi: Satu kategori bisa memiliki banyak produk
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
