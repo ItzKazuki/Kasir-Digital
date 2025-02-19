12501c1933e44b7317ff29443f4cf8ac7d0fd790
ItzKazuki <ibnu235729@gmail.com>
2025-02-19 19:38:24 +0700

    feat: add OrderDetail-Order and OrderDetail-Product relationship

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'total_price',
+    ];
+
+    // Relasi: Order Detail milik satu order
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    // Relasi: Order Detail berisi satu produk
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
+
