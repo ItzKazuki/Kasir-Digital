1d27eb10e1305a81e30035c7a751777fe00f5ec8
ItzKazuki <ibnu235729@gmail.com>
2025-02-19 19:39:29 +0700

    feat: add Transaction-Order and Transaction-Member relationship

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'member_id',
+        'total_price',
+        'payment_method',
+        'payment_status',
+    ];
+
+    // Relasi: Transaksi terkait dengan satu order
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    // Relasi: Transaksi bisa memiliki satu member (opsional)
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
 }
