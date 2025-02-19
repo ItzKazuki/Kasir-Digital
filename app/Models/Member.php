5b2e2ba2ab1fae30b7fd5ad21a01fccf19e6287c
ItzKazuki <ibnu235729@gmail.com>
2025-02-19 19:29:47 +0700

    feat: add Member-Order and Member-Transaction relationship

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'full_name',
+        'no_telp',
+        'point',
+        'email',
+        'status',
+    ];
+
+    // Relasi: Member bisa memiliki banyak order
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    // Relasi: Member bisa memiliki banyak transaksi
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
+
