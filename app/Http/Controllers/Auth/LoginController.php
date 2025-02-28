d93faae19602c0cf028bb0057438c8da8fa48296
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-02-28 18:26:46 +0700

    feat: add  to auth.blade.php for make a beautiful title

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,7 +10,9 @@ class LoginController extends Controller
 {
     public function login()
     {
-        return view('auth.login');
+        $title = "Login";
+
+        return view('auth.login', compact('title'));
     }
 
     public function authenticate(Request $request)
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kasir Digital | Login</title>
+    <title>Kasir Digital | {{ $title }}</title>
 
     {{-- style internal or external --}}
     @vite('resources/css/app.css')
