cc21e45ec5891b004e65eabf1f35c90573140724
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-01 07:35:10 +0700

    feat: add feature create and edit product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,8 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Wavey\Sweetalert\Sweetalert;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Discount;
 use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
@@ -26,7 +28,9 @@ class ProductController extends Controller
     public function create()
     {
         $title = "Create Products";
-        return view('dashboard.products.create', compact('title'));
+        $categories = Category::all();
+        $discounts = Discount::all();
+        return view('dashboard.products.create', compact('title', 'categories', 'discounts'));
     }
 
     /**
@@ -34,7 +38,40 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $productDetail = $request->validate([
+            'name_product' => 'required|min:3',
+            'category_id' => 'required|exists:categories,id',
+            'price' => 'required|numeric',
+            'stock' => 'required',
+            'expired_at' => 'date',
+            'product_img' => 'required|mimes:png,jpg|max:2048',
+            'discount_id' => ''
+        ]);
+
+        // upload image products
+        $imageName = str_replace(' ', '-', $productDetail['name_product']) . '-' . date('dmyHis') . '.' . $request->file('product_img')->extension();
+        Storage::putFileAs('static/images/products', $request->file('product_img'), $imageName);
+
+        $dataProduk = [
+            'name' => $productDetail['name_product'],
+            'category_id' => $productDetail['category_id'],
+            'price' => $productDetail['price'],
+            'stock' => $productDetail['stock'],
+            'image_url' => $imageName
+        ];
+
+        if($productDetail['discount_id'] != null) {
+            $dataProduk['discount_id'] = $productDetail['discount_id'];
+        }
+
+        if($productDetail['expired_at'] != null) {
+            $dataProduk['expired_at'] = $productDetail['expired_at'];
+        }
+
+        Product::create($dataProduk);
+
+        Sweetalert::success('berhasil menabah produk baru', 'Tambah Produk Berhasil!');
+        return redirect()->route('dashboard.products.index');
     }
 
     /**
@@ -51,7 +88,9 @@ class ProductController extends Controller
     public function edit(Product $product)
     {
         $title = "Edit Products";
-        return view('dashboard.products.edit', compact('title', 'product'));
+        $categories = Category::all();
+        $discounts = Discount::all();
+        return view('dashboard.products.edit', compact('title', 'product', 'categories', 'discounts'));
     }
 
     /**
@@ -59,7 +98,46 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $productDetail = $request->validate([
+            'name_product' => 'required|min:3',
+            'category_id' => 'required|numeric|exists:categories,id',
+            'price' => 'required|numeric',
+            'stock' => 'required',
+            'expired_at' => 'date',
+            'product_img' => 'required|mimes:png,jpg|max:2048',
+            'discount_id' => ''
+        ]);
+
+        $product->name = $productDetail['name_product'];
+        $product->category_id = $productDetail['category_id'];
+        $product->price = $productDetail['price'];
+        $product->stock = $productDetail['stock'];
+
+        if($request->file('product_img')) {
+            // delete old image
+            if($product->image_url != null) {
+                Storage::delete('static/images/products/' . $product->image_url);
+            }
+
+            // store new image
+            // upload image products
+            $imageName = str_replace(' ', '-', $productDetail['name_product']) . '-' . date('dmyHis') . '.' . $request->file('product_img')->extension();
+            Storage::putFileAs('static/images/products', $request->file('product_img'), $imageName);
+            $product->image_url = $imageName;
+        }
+
+        if($productDetail['discount_id'] != null) {
+            $product->discount_id = $productDetail['discount_id'];
+        }
+
+        if($productDetail['expired_at'] != null) {
+            $product->expired_at = $productDetail['expired_at'];
+        }
+
+        $product->save();
+
+        Sweetalert::success('berhasil menabah produk baru', 'Tambah Produk Berhasil!');
+        return redirect()->route('dashboard.products.index');
     }
 
     /**
