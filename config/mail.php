75ca0d4859893fba0b2838e4c57a83c535705880
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-04 17:43:24 +0700

    feat: add another mail for information

--- config/mail.php
+++ config/mail.php
@@ -49,6 +49,21 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'info' => [
+            'transport' => 'smtp',
+            'scheme' => env('MAIL_SCHEME'),
+            'url' => env('MAIL_URL'),
+            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'port' => env('MAIL_PORT', 2525),
+            'username' => env('INFO_MAIL_USERNAME'),
+            'password' => env('INFO_MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'form' => [
+                'address' => env('INFO_MAIL_USERNAME')
+            ]
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
