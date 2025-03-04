e8874f6fba4119d974b5aeba4a2a0b1588121e15
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-04 22:54:01 +0700

    feat: integrate Font Awesome and SweetAlert2 for enhanced UI components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+// TODO: fix this sweetalert using local sweetalert
+// import Swal from 'sweetalert2';
+// window.Swal = Swal;
