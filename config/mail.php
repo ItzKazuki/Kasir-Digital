8b8af308d544c89807150f81525c724f80c3eb14
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-04 20:45:56 +0700

    fix: update mail configuration environment variables for consistency

--- config/mail.php
+++ config/mail.php
@@ -55,12 +55,13 @@ return [
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
-            'username' => env('INFO_MAIL_USERNAME'),
-            'password' => env('INFO_MAIL_PASSWORD'),
+            'username' => env('MAIL_INFO_USERNAME'),
+            'password' => env('MAIL_INFO_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-            'form' => [
-                'address' => env('INFO_MAIL_USERNAME')
+            'from' => [
+                'address' => env('MAIL_INFO_FROM_ADDRESS'),
+                'name' => env('MAIL_FROM_NAME', 'Example'),
             ]
         ],
 
