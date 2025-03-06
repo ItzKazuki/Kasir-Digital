fb3da173b76ddfbc3d482b8487c9b3f5c6c95492
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-07 01:37:36 +0700

    feat: implement PDF generation for transaction struk using a dedicated trait

---
+++ app/GeneratesStrukPdf.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use App\Models\Transaction;
+use Spatie\LaravelPdf\Facades\Pdf;
+use Spatie\Browsershot\Browsershot;
+use Spatie\LaravelPdf\Enums\Orientation;
+
+trait GeneratesStrukPdf
+{
+    public function generateStrukPdf(Transaction $transaction)
+    {
+        $height = 150;
+
+        if ($transaction->order->orderDetails()->count() > 1) {
+            $height = $height + ($transaction->order->orderDetails()->count() * 5);
+        }
+
+        Pdf::view('dashboard.transactions.struk', compact('transaction'))
+            ->withBrowsershot(function (Browsershot $browsershot) {
+                $browsershot
+                    ->noSandbox()
+                    ->setNodeBinary(config('app.node.path'))
+                    ->setNpmBinary(config('app.node.npm'));
+            })
+            ->disk('local')
+            ->orientation(Orientation::Portrait)
+            ->paperSize(96, $height, 'mm')
+            ->save('/static/struk/' . $transaction->invoice_number . '.pdf');
+    }
+}
