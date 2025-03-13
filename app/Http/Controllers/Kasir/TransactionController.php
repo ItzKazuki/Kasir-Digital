fa7dcd87aa7fb01f734c321a8a5522797c510114
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-13 15:45:06 +0700

    feat: enhance TransactionController to filter transactions by user and date range

--- app/Http/Controllers/Kasir/TransactionController.php
+++ app/Http/Controllers/Kasir/TransactionController.php
@@ -2,17 +2,43 @@
 
 namespace App\Http\Controllers\Kasir;
 
-use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TransactionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $title = "Transactions";
+
+        $query = Transaction::with('order.member');
+
+        // get all transactions that belong to the authenticated user
+        $query->whereHas('order', function ($q) use ($request) {
+            $q->where('user_id', $request->user()->id);
+        });
+
+        if ($request->start_date && $request->end_date) {
+            $query->whereHas('order', function ($q) use ($request) {
+                $q->whereBetween('order_date', [$request->start_date, $request->end_date]);
+            });
+        }
+
+        if ($request->payment_status) {
+            $query->where('payment_status', $request->payment_status);
+        }
+
+        $transactions = $query->paginate(10)->appends(request()->query());
+
+        $income = Transaction::where('payment_status', 'paid')->sum('cash');
+        $outcome = Transaction::where('payment_status', 'paid')->sum('cash_change');
+
+        return view('dashboard.kasir.transactions.index', compact('title', 'transactions', 'income', 'outcome'));
     }
 
     /**
