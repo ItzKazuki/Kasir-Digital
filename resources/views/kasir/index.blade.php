4fc9e6644f6a09e75670540f01030215d67c52bd
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-15 05:43:35 +0700

    feat: update page titles and footer to use application name dynamically

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kasie Digital | Kasir</title>
+    <title>{{ config('app.name') }} | Kasir</title>
 
     {{-- style internal or external --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
