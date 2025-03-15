1c0fff30a7005489de28a2e166dabe8811cfd7eb
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-15 07:21:05 +0700

    feat: implement ShouldQueue interface for notification classes

--- app/Notifications/MemberCreatedNotification.php
+++ app/Notifications/MemberCreatedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class MemberCreatedNotification extends Notification
+class MemberCreatedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
