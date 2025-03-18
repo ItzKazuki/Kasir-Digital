2099f1ea6a969c8c0314748970b2cd56c3edb346
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-18 10:27:51 +0700

    feat: install api sanctum laravel

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Member extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $fillable = [
         'full_name',
