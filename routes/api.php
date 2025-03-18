8daa8ad13366cc9c5b8697833229cba81f81e8f1
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-03-18 10:57:13 +0700

    feat: add basic controller for api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// make path like this
+// mywebsite.com/api/v1/products
+
+Route::group(['middleware' => ['api'],'prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'auth'], function() {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
+    });
+
+    Route::group(['middleware' => ['auth:sanctum']], function() {
+        Route::apiResource('products', ProductController::class);
+        Route::apiResource('categories', CategoryController::class);
+        Route::apiResource('transactions', TransactionController::class);
+
+        Route::get('profile', [AuthController::class, 'profile']);
+        Route::get('auth/logout', [AuthController::class, 'logout']);
+    });
+});
