b3fb082603db45ffae31c8fa69437464166a10be
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-04-24 11:15:10 +0700

    feat: improve layout responsiveness for mobile devices and adjust print styles

--- resources/views/layouts/struk.blade.php
+++ resources/views/layouts/struk.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
-    <meta name="viewport" content="width=device-width, initial-scale=0.8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Invoice {{ $transaction->invoice_number }} | {{ config('app.name') }}</title>
     <style>
         body {
