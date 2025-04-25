92f95d4b1c2b9ac11c3a3ec89042f5346f895765
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-04-25 15:40:52 +0700

    fix: remove unused seeders from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSedder::class,
-            MemberSeeder::class,
             CategorySeeder::class,
         ]);
     }
